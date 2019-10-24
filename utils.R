dea3af7430dc14ab3fd67f5cb55f18d04e049af9
EmmaDann <e.dann@students.uu.nl>
2019-10-24 15:43:44 +0000

    Joint clustering analysis

--- utils.R
+++ utils.R
@@ -2,6 +2,8 @@
 ### Utils ###
 #############
 
+library(RColorBrewer)
+
 #' Convert output of integration method to Seurat object
 #' @param intOut output of integration
 #' @param int.features features used for integration (HVGs in reference dataset)
@@ -10,4 +12,14 @@ intOutput2seurat <- function(intOut, int.features, reference="RNA"){
   int.seu <- as.Seurat(intOut[[paste0("integrated.", reference)]], counts=NULL, data="normcounts")
   int.seu@assays$RNA@var.features <- int.features
   return(int.seu)
-  }
+}
+
+
+### Plotting utils ###
+
+brewer_palette_4_values <- function(vec, palette, seed=42){
+  set.seed(seed)
+  pal=suppressWarnings(colorRampPalette(brewer.pal(9, palette))(length(vec)))
+  names(pal) <- sample(vec)
+  return(pal)
+}
