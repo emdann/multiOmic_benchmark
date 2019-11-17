b9cf70d53eb68f2f82ce81f7b22a64512121730f
EmmaDann <e.dann@students.uu.nl>
2019-11-17 17:50:10 +0000

    Testing MATCHER

--- preprocess/selectFeatures.R
+++ preprocess/selectFeatures.R
@@ -5,7 +5,8 @@
 library(Seurat)
 
 HVG_Seurat <- function(sce, nfeatures=2000){
-  HVGs <- VariableFeatures(FindVariableFeatures(as.Seurat(sce), nfeatures = nfeatures))
+  HVGs <- VariableFeatures(FindVariableFeatures(as.Seurat(sce), nfeatures = nfeatures, 
+                                                selection.method="mvp", dispersion.cutoff=c(0.5, 100000), mean.cutoff=c(0.0125, 6)))
 }
 
 
