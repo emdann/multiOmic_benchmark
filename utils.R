c726556f3d7d0b652a57f311766367fa4fa42146
EmmaDann <e.dann@students.uu.nl>
2019-11-20 15:49:59 +0000

    UMAPs by cluster

--- utils.R
+++ utils.R
@@ -36,17 +36,33 @@ eucl_distance <- function(p,q){
 
 brewer_palette_4_values <- function(vec, palette, seed=42){
   set.seed(seed)
-  pal=suppressWarnings(colorRampPalette(brewer.pal(9, palette))(length(vec)))
+  pal=suppressWarnings(colorRampPalette(brewer.pal(12, palette))(length(vec)))
   # names(pal) <- sample(vec)
   return(pal)
 }
 
-DimPlotCluster <- function(atac.seu, annotation_col, cluster, label=NULL){
+DimPlotCluster <- function(atac.seu, annotation_col, cluster, label=NULL, reduct="umap.snap"){
   if (is.null(label)) {
     label <- cluster
   }
   highlight = which(atac.seu@meta.data[,annotation_col]==cluster)
-  DimPlot(atac.seu, reduction = "umap.snap",cells.highlight = highlight, cols.highlight = "red", pt.size = 0.02, sizes.highlight = 0.1) +
+  DimPlot(atac.seu, reduction = reduct, cells.highlight = highlight, cols.highlight = "red", pt.size = 0.02, sizes.highlight = 0.1) +
     guides(color="none") +
-    ggtitle(label = label)
+    ggtitle(label = label) +
+    theme(axis.ticks = element_blank(), axis.text = element_blank())
+}
+
+
+FeaturePlotCluster <- function(atac.seu, annotation_col, feature_col, cluster, label=NULL, reduct="umap.snap"){
+  if (is.null(label)) {
+    label <- cluster
+  }
+  atac.seu.copy <- atac.seu
+  grey_cells = which(atac.seu.copy@meta.data[,annotation_col]!=cluster)
+  atac.seu.copy@meta.data[grey_cells,feature_col] <- NA
+  FeaturePlot(atac.seu.copy, reduction = reduct, feature=feature_col, order=TRUE) +
+    scale_color_viridis_c(na.value = "grey50") +
+    # guides(color="none") +
+    ggtitle(label = label) +
+    theme(axis.ticks = element_blank(), axis.text = element_blank())
 }
